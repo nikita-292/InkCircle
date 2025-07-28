2aa09cf17d3af2f4f437b81b6dcc44e4170136ee
nikita-292 <145786206+nikita-292@users.noreply.github.com>
2025-07-28 17:39:37 +0530

    new cors

--- api/app.js
+++ api/app.js
@@ -58,25 +58,26 @@ const app = express();
 //   next()
 // })
 
-
-const allowedOrigins = [process.env.FRONTEND_URL];
+const allowedOrigins = [process.env.FRONTEND_URL]; // must match exactly
 
 app.use((req, res, next) => {
   const origin = req.headers.origin;
-  console.log('Incoming request from origin:', origin);
   if (allowedOrigins.includes(origin)) {
     res.setHeader('Access-Control-Allow-Origin', origin);
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Origin,X-Requested-With,Content-Type,Accept,Authorization'
+    );
   }
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
 
   if (req.method === 'OPTIONS') {
     return res.sendStatus(200);
   }
+
   next();
 });
-
 
 
 // Then use other middleware
