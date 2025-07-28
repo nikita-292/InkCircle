d8ab7c2a8dc1424783d797555efd5c6fa76fe74e
nikita-292 <145786206+nikita-292@users.noreply.github.com>
2025-07-28 11:50:07 +0530

    viteconfig

--- client/vite.config.js
+++ client/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
       "/api": {
         target: "https://inkcircle-host.onrender.com",
         changeOrigin: true,
-        secure: false,
+        secure: true,
       },
     },
   },
