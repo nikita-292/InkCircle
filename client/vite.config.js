fea4c5d456a10aab5c946ca0964c9bbc3358e893
nikita-292 <145786206+nikita-292@users.noreply.github.com>
2025-07-30 23:11:18 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://inkcircle-host.onrender.com",
+        target: "http://localhost:3000",
         changeOrigin: true,
         secure: true,
       },
